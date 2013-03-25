fa91ee912f054d1af67ce857574fffc153356503
EragonJ <eragonj@eragonj.me>
2013-03-25 22:43:00 +0800

    override console object for IE

--- src/trip.js
+++ src/trip.js
@@ -45,9 +45,25 @@
             ESC : 27,
             SPACE : 32
         };
+
+        this.console = window.console || {};
     };
 
     Trip.prototype = {
+
+        preInit : function() {
+
+            // override console object for IE
+            if ( typeof this.console === "undefined" ) {
+
+                var self = this,
+                    methods = ['log', 'warn', 'debug', 'info', 'error'];
+
+                $.each(methods, function(i, methodName) {
+                    self.console[methodName] = $.noop;
+                });
+            }
+        },
 
         // TODO: implement expose
         showExpose : function( $sel ) {
@@ -328,7 +344,7 @@
             if ( typeof o.sel === 'undefined' ||
                     typeof o.content === 'undefined' ) {
 
-                console.warn("Your tripData is not valid in obj :" + o +".");
+                this.console.warn("Your tripData is not valid in obj :" + o +".");
                 return false;
             }
         },
@@ -452,6 +468,7 @@
         },
 
         init : function() {
+            this.preInit();
             this.bindKeyEvents();
 
             // set refs
