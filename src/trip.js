eb1f5e5e5c4771d867204bb4008b23cac42a75f4
EragonJ <eragonj@eragonj.me>
2014-08-28 12:28:37 +0800

    change some tabs to spaces to make code look nice

--- src/trip.js
+++ src/trip.js
@@ -80,18 +80,18 @@
       animation: 'tada',
 
       // customizable HTML
-			tripBlockHTML: [
-				'<div class="trip-block">',
-					'<a href="#" class="trip-close"></a>',
+      tripBlockHTML: [
+        '<div class="trip-block">',
+          '<a href="#" class="trip-close"></a>',
           '<div class="trip-header"></div>',
-					'<div class="trip-content"></div>',
-					'<div class="trip-progress-wrapper">',
-						'<div class="trip-progress-bar"></div>',
-						'<a href="#" class="trip-prev"></a>',
-						'<a href="#" class="trip-next"></a>',
-					'</div>',
-				'</div>'
-			]
+          '<div class="trip-content"></div>',
+          '<div class="trip-progress-wrapper">',
+            '<div class="trip-progress-bar"></div>',
+            '<a href="#" class="trip-prev"></a>',
+            '<a href="#" class="trip-next"></a>',
+          '</div>',
+        '</div>'
+      ]
     }, userOptions);
 
     this.tripData = tripData;
