3b3b827905503cdff8a5068237b25c48c367d88c
EragonJ <eragonj@eragonj.me>
2015-02-16 23:33:05 +0800

    fixed #87, we now can define trips from HTML

---
+++ src/trip._header_.js
@@ -0,0 +1,13 @@
+/**
+ *  Trip.js
+ *
+ *  This is a jQuery plugin that can help you customize your tutorial trip
+ *  with full flexibilities.
+ *
+ *  Version: 2.0.2
+ *
+ *  Author: EragonJ <eragonj@eragonj.me>
+ *  Blog: http://eragonj.me
+ *
+ *  @preserve
+ */
