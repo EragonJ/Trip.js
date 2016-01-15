0f53dccfa0c69417f80774378cb98a1376a1f358
EragonJ <eragonj@eragonj.me>
2016-01-15 18:20:39 +0800

    merge from master

--- dist/trip.js
+++ dist/trip.js
@@ -4,7 +4,7 @@
  *  This is a jQuery plugin that can help you customize your tutorial trip
  *  with full flexibilities.
  *
- *  Version: 3.1.2
+ *  Version: 3.1.3
  *
  *  Author: EragonJ <eragonj@eragonj.me>
  *  Blog: http://eragonj.me
@@ -1003,6 +1003,25 @@ return /******/ (function(modules) { // webpackBootstrap
 	   */
 	  setTripBlockPosition: function(o, horizontalOrVertical) {
 	    var $tripBlock = this.$tripBlock;
+
+	    //Styles need to be reset before capturing outer height and width
+	    if (horizontalOrVertical === 'horizontal') {
+	        // reset styles first
+	        $tripBlock.css({
+	            left: '',
+	            right: '',
+	            marginLeft: '',
+	        });
+	    }
+	    else if (horizontalOrVertical === 'vertical') {
+	        // reset styles first
+	        $tripBlock.css({
+	            top: '',
+	            bottom: '',
+	            marginTop: '',
+	        });
+	    }
+
 	    var $sel = $(o.sel);
 	    var selWidth = $sel && $sel.outerWidth();
 	    var selHeight = $sel && $sel.outerHeight();
@@ -1046,13 +1065,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }
 
 	    if (horizontalOrVertical === 'horizontal') {
-	      // reset styles first
-	      $tripBlock.css({
-	        left: '',
-	        right: '',
-	        marginLeft: '',
-	      });
-
 	      switch (o.position) {
 	        case 'screen-center':
 	          $tripBlock.css({
@@ -1081,12 +1093,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	      }
 	    }
 	    else if (horizontalOrVertical === 'vertical') {
-	      // reset styles first
-	      $tripBlock.css({
-	        top: '',
-	        bottom: '',
-	        marginTop: '',
-	      });
 
 	      switch (o.position) {
 	        case 'screen-center':
