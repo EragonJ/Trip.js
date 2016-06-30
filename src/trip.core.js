1a4f4684fa043a2988b08c2c8e37b2934765ac49
EragonJ <eragonj@eragonj.me>
2016-06-30 22:13:08 +0800

    merge from master

--- src/trip.core.js
+++ src/trip.core.js
@@ -3,16 +3,7 @@ module.exports = Trip;
 var $ = require('jquery');
 var TripParser = require('./trip.parser');
 var TripUtils = require('./trip.utils');
-
-var CHECKED_ANIMATIONS = [
-  'flash', 'bounce', 'shake', 'tada',
-  'fadeIn', 'fadeInUp', 'fadeInDown',
-  'fadeInLeft', 'fadeInRight', 'fadeInUpBig', 'fadeInDownBig',
-  'fadeInLeftBig', 'fadeInRightBig', 'bounceIn', 'bounceInDown',
-  'bounceInUp', 'bounceInLeft', 'bounceInRight', 'rotateIn',
-  'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft',
-  'rotateInUpRight'
-];
+var TripAnimation = require('./trip.animation');
 
 /**
  * Trip
@@ -435,13 +426,14 @@ Trip.prototype = {
    * next API, which will jump to next the trip.
    *
    * @memberOf Trip
+   * @param {Number} tripIndex
    * @type {Function}
    * @public
    */
-  next: function() {
+  next: function(tripIndex) {
     var that = this;
-    // We have to make sure we can go next first,
-    // if not, let's just re-run
+    var useDifferentIndex = !isNaN(tripIndex);
+
     if (!this.canGoNext()) {
       return this.run();
     }
@@ -456,6 +448,15 @@ Trip.prototype = {
     var tripEndDefer = tripEnd(this.tripIndex, tripObject);
 
     $.when(tripEndDefer).then(function() {
+      if (useDifferentIndex) {
+        if (that.timer) {
+          that.timer.stop();
+        }
+        that.setIndex(tripIndex);
+        that.run();
+        return;
+      }
+
       if (that.isLast()) {
         that.doLastOperation();
       }
@@ -785,6 +786,11 @@ Trip.prototype = {
     return canGoNext;
   },
 
+  setIndex: function(tripIndex) {
+    tripIndex = Math.max(0, Math.min(tripIndex, this.tripData.length - 1));
+    this.tripIndex = tripIndex;
+  },
+
   /**
    * We can call this method to increase tripIndex because we are not allowed
    * to manipualte the value directly.
@@ -793,13 +799,7 @@ Trip.prototype = {
    * @type {Function}
    */
   increaseIndex: function() {
-    if (this.tripIndex >= this.tripData.length - 1) {
-      // how about hitting the last item ?
-      // do nothing
-    }
-    else {
-      this.tripIndex += 1;
-    }
+    this.setIndex(this.tripIndex + 1);
   },
 
   /**
@@ -810,13 +810,7 @@ Trip.prototype = {
    * @type {Function}
    */
   decreaseIndex: function() {
-    if (this.tripIndex <= 0) {
-      // how about hitting the first item ?
-      // do nothing
-    }
-    else {
-      this.tripIndex -= 1;
-    }
+    this.setIndex(this.tripIndex - 1);
   },
 
   /**
@@ -1067,7 +1061,7 @@ Trip.prototype = {
    */
   addAnimation: function(o) {
     var animation = o.animation || this.settings.animation;
-    if ($.inArray(animation, CHECKED_ANIMATIONS) >= 0) {
+    if (TripAnimation.has(animation)) {
       this.$tripBlock.addClass('animated');
       this.$tripBlock.addClass(animation);
     }
@@ -1080,7 +1074,7 @@ Trip.prototype = {
    * @type {Function}
    */
   removeAnimation: function() {
-    this.$tripBlock.removeClass(CHECKED_ANIMATIONS.join(' '));
+    this.$tripBlock.removeClass(TripAnimation.getAllInString());
     this.$tripBlock.removeClass('animated');
   },
 
