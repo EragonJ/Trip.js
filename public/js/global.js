0e8820684e5e0f7b4dba35d5bbd2051aa00a8db5
EragonJ <eragonj@eragonj.me>
2016-08-05 22:47:02 +0800

    Deployed 240083e with MkDocs version: 0.15.3

--- public/js/global.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  // highlight code
-  hljs.initHighlightingOnLoad();
-
-  // UI
-  $('#top-header .nav li').hover(function() {
-    $(this).toggleClass('active');
-  });
-});
--- src/themes/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  black: require('./default'),
-  dark: require('./default'),
-  white: require('./default'),
-  yeti: require('./default'),
-  default: require('./default'),
-  minimalism: require('./minimalism')
-};
